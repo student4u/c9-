49b219d1d5531b06d59b77d0ce4cac4c5bbe8c8f
student4u <77529499+student4u@users.noreply.github.com>
2021-11-30 16:48:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+  box.shapeColor = "red"
+
+}
+
+function draw() 
+{
+   background("lightblue");
+
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x + 5
+  }
+
+if(keyIsDown(LEFT_ARROW)){
+  box.position.x = box.position.x - 5
+}
+
+  drawSprites();
+}
+
+
+
+
